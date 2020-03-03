43bf777157a7a498ffa33fbaceb5af9eea6fa555
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-03-03 09:05:47 -0600

    Fetch the message

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+    ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -25,4 +25,12 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+function getMessageUsingArrowFunctions() {
+  fetch('/data')
+    .then(response => response.text())
+    .then((message) => {
+      document.getElementById('message-container').innerHTML = message;
+    });
 }
