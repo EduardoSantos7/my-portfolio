9a18e03727d7f26d4d7631119a07f9d65275882e
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-03-05 23:17:58 -0600

    Add Map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -45,3 +45,11 @@ function createListElement(text) {
   liElement.innerText = text;
   return liElement;
 }
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
