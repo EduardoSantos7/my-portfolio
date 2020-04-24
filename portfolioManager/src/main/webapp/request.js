7b2c6724912c0b0998b37b9c218b7cefafc69c5a
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-04-23 21:33:19 -0500

    getSentiment

--- portfolioManager/src/main/webapp/request.js
+++ portfolioManager/src/main/webapp/request.js
@@ -1,8 +1,10 @@
-const Http = new XMLHttpRequest();
-const url = '/sentiment';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
+function getSentiment(message) {
+    const Http = new XMLHttpRequest();
+    const url = '/sentiment';
+    Http.open("GET", url);
+    Http.send();
+    
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
 }
