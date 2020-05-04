bac763d8e61462099baed0f3562dc035616a088c
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-05-04 12:44:17 -0500

    delete char

--- portfolioManager/src/main/webapp/script.js
+++ portfolioManager/src/main/webapp/script.js
@@ -16,30 +16,6 @@
 let companies = []
 
 google.charts.load('current', { 'packages': ['corechart', 'line'] });
-google.charts.setOnLoadCallback(drawChart);
-//google.charts.setOnLoadCallback(drawBackgroundColor);
-
-/** Creates a chart and adds it to the page. */
-function drawChart() {
-  const data = new google.visualization.DataTable();
-  data.addColumn('string', 'Animal');
-  data.addColumn('number', 'Count');
-  data.addRows([
-    ['Lions', 10],
-    ['Tigers', 5],
-    ['Bears', 15]
-  ]);
-
-  const options = {
-    'title': 'Zoo Animals',
-    'width': 500,
-    'height': 400
-  };
-
-  const chart = new google.visualization.PieChart(
-    document.getElementById('chart-container'));
-  chart.draw(data, options);
-}
 
 function drawBackgroundColor(marketCapitalData) {
   if (!marketCapitalData) return;
